752438cc015c536966148b83bdfa07736bd0064c
mutterer <jerome.mutterer@gmail.com>
2020-10-09 23:01:23 +0200

    add open image from zenodo
    I check for .tif extension for now.

--- extension/main.js
+++ extension/main.js
@@ -1,15 +1,19 @@
 switch (location.hostname) {
-
+  
 case 'gist.github.com':
-    this.isButtonInsertedGithubGist(document.URL);
-    document.addEventListener('pjax:end', () => isButtonInsertedGithubGist(document.URL));
-    break;
+  this.isButtonInsertedGithubGist(document.URL);
+  document.addEventListener('pjax:end', () => isButtonInsertedGithubGist(document.URL));
+  break;
 case 'github.com':
-    this.isButtonInsertedGithub(document.URL);
-    document.addEventListener('pjax:end', () => isButtonInsertedGithub(document.URL));
-    break;
-  default:
-    break;
+  this.isButtonInsertedGithub(document.URL);
+  document.addEventListener('pjax:end', () => isButtonInsertedGithub(document.URL));
+  break;
+case 'zenodo.org':
+  this.isButtonInsertedZenodo(document.URL);
+  document.addEventListener('pjax:end', () => isButtonInsertedZenodo(document.URL));
+  break;
+default:
+  break;
 }
 function isButtonInsertedGithubGist(url) {
 	var buttons = document.getElementsByClassName("btn-sm");
@@ -17,7 +21,7 @@ function isButtonInsertedGithubGist(url) {
 	for (var i = 0; i < buttons.length; i++) {
 	  if (buttons[i].innerText=="Raw") b=i;
 	}
-    if ((b>-1)&&(buttons[b].href.endsWith(".ijm")))
+	if ((b>-1)&&(buttons[b].href.endsWith(".ijm")))
 	{
     const runGistInImageJdotJS = document.createElement('a');
     runGistInImageJdotJS.innerHTML = 'Run in ImageJ.JS';
@@ -40,7 +44,7 @@ function isButtonInsertedGithub(url) {
 	for (var i = 0; i < buttons.length; i++) {
 	  if (buttons[i].innerText=="Raw") b=i;
 	}
-    if ((b>-1)&&(buttons[b].href.endsWith(".ijm")))
+	if ((b>-1)&&(buttons[b].href.endsWith(".ijm")))
 	{
     const runGistInImageJdotJS = document.createElement('a');
     runGistInImageJdotJS.innerHTML = 'Run in ImageJ.JS';
@@ -56,4 +60,27 @@ function isButtonInsertedGithub(url) {
   } else {
     return false;
   }
+}
+
+function isButtonInsertedZenodo(url) {
+	var buttons = document.getElementsByClassName("btn btn-xs btn-default");
+	for (var i = 0; i < buttons.length; i++) {
+	  if (buttons[i].innerText.indexOf('Download')>-1) {
+	    imgUrl = buttons[i].href;
+	    imgUrl = imgUrl.replace('?download=1','');
+	    if (imgUrl.toLowerCase().endsWith('.tif')) {
+	      const openInImageJdotJS = document.createElement('a');
+	      openInImageJdotJS.innerHTML = 'Open in ImageJ.JS';
+	      openInImageJdotJS.setAttribute('class', 'btn btn-xs btn-default');
+	      openInImageJdotJS.setAttribute('target','_blank');
+	      openInImageJdotJS.setAttribute('href', 'https://ij.imjoy.io/?open='+imgUrl);
+	      try {
+	        buttons[i].parentNode.appendChild(openInImageJdotJS);
+	      } catch (error) {
+	        return false;
+	      }
+ 	    }
+	  }
+	}
+	return true;
 }
