7dc8e8fbbeb83866d19a2caabac8b9eb6074c7b8
mutterer <jerome.mutterer@gmail.com>
2020-10-09 11:28:22 +0200

    add github repos button

--- extension/main.js
+++ extension/main.js
@@ -1,10 +1,38 @@
 switch (location.hostname) {
-  case 'gist.github.com':
+
+case 'gist.github.com':
+    this.isButtonInsertedGithubGist(document.URL);
+    document.addEventListener('pjax:end', () => isButtonInsertedGithubGist(document.URL));
+    break;
+case 'github.com':
     this.isButtonInsertedGithub(document.URL);
     document.addEventListener('pjax:end', () => isButtonInsertedGithub(document.URL));
     break;
   default:
     break;
+}
+function isButtonInsertedGithubGist(url) {
+	var buttons = document.getElementsByClassName("btn-sm");
+	let b=-1;
+	for (var i = 0; i < buttons.length; i++) {
+	  if (buttons[i].innerText=="Raw") b=i;
+	}
+    if ((b>-1)&&(buttons[b].href.endsWith(".ijm")))
+	{
+    const runGistInImageJdotJS = document.createElement('a');
+    runGistInImageJdotJS.innerHTML = 'Run in ImageJ.JS';
+    runGistInImageJdotJS.setAttribute('class', 'btn btn-sm ');
+    runGistInImageJdotJS.setAttribute('target','_blank');
+    runGistInImageJdotJS.setAttribute('href', "https://ij.imjoy.io/?run="+buttons[b].href.replace("https://gist.github.com/","https://gist.githubusercontent.com/"));
+    try {
+      buttons[b].parentNode.appendChild(runGistInImageJdotJS);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  } else {
+    return false;
+  }
 }
 function isButtonInsertedGithub(url) {
 	var buttons = document.getElementsByClassName("btn-sm");
@@ -18,7 +46,7 @@ function isButtonInsertedGithub(url) {
     runGistInImageJdotJS.innerHTML = 'Run in ImageJ.JS';
     runGistInImageJdotJS.setAttribute('class', 'btn btn-sm ');
     runGistInImageJdotJS.setAttribute('target','_blank');
-    runGistInImageJdotJS.setAttribute('href', "https://ij.imjoy.io/?run="+buttons[b].href.replace("https://gist.github.com/","https://gist.githubusercontent.com/"));
+    runGistInImageJdotJS.setAttribute('href', 'https://ij.imjoy.io/?run='+url);
     try {
       buttons[b].parentNode.appendChild(runGistInImageJdotJS);
       return true;
