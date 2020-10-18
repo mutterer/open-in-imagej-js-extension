81a669334ab543f87cb9ff44cdc2878965f34819
mutterer <jerome.mutterer@gmail.com>
2020-10-18 19:51:15 +0200

    add links to HPA pages.

--- extension/main.js
+++ extension/main.js
@@ -12,6 +12,10 @@ case 'zenodo.org':
   this.isButtonInsertedZenodo(document.URL);
   document.addEventListener('pjax:end', () => isButtonInsertedZenodo(document.URL));
   break;
+case 'www.proteinatlas.org':
+  this.isButtonInsertedHPA(document.URL);
+  document.addEventListener('pjax:end', () => isButtonInsertedHPA(document.URL));
+  break;
 default:
   break;
 }
@@ -88,6 +92,38 @@ function isButtonInsertedZenodo(url) {
         }
       }
     }
+  }
+  return true;
+}
+
+function isButtonInsertedHPA(url) {
+  var element = document.getElementsByClassName("imageinfo");
+  for (var i = 0; i < element.length; i++) {
+      if (i==0) console.log(element[i].parentElement.firstElementChild);
+      const openInImageJdotJS = document.createElement('a');
+        const imagejIcon = document.createElement('img');
+        imagejIcon.src = 'https://ij.imjoy.io/assets/icons/chrome/chrome-extensionmanagementpage-48-48.png';
+        imagejIcon.style.height = '16px';
+        openInImageJdotJS.appendChild(imagejIcon);
+        openInImageJdotJS.innerHTML += 'Open in ImageJ.JS';
+        openInImageJdotJS.style.color = "#0366d6";
+        openInImageJdotJS.setAttribute('class', 'ijjs');
+        openInImageJdotJS.setAttribute('target','_blank');
+        openInImageJdotJS.onclick = function() { 
+          // the correct link can only be captured onClick, 
+          //as the images can be changed from within the page.
+          var ele = document.getElementsByClassName("ijjs");
+          for (var i = 0; i < element.length; i++) {
+            ele[i].setAttribute('href', 'https://ij.imjoy.io/?open='+ele[i].parentElement.firstElementChild.href);      
+          }
+          return true;
+        }; 
+        try {
+          element[i].parentNode.appendChild(document.createElement('br'));
+          element[i].parentNode.appendChild(openInImageJdotJS);
+        } catch (error) {
+          return false;
+        }
   }
   return true;
 }
